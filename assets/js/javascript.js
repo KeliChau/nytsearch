713d319e1319c13f3ed654b8ed79b6ed809c6a32
kelichau <tchau2@outlook.com>
2017-11-29 13:56:57 -0500

    Added div for search results

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -10,6 +10,8 @@
       url: url,
       method: 'GET',
     }).done(function(result) {
+    $("#search").text(JSON.stringify(response));
+    renderButtons();
     
     console.log(result);
     
@@ -18,10 +20,15 @@
       });
     });
 
+    function renderButtons(){
+      $("#submit").empty();
+    }
 
       var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
         url += '?' + $.param({
         'api-key': "8db7e5f2f14e40afb811ff718ef5bd56"
       });
       console.log(url);
+
+
   })

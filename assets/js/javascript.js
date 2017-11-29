50fc27b417cf95d5dc44c70eb58112778c8b5a10
kelichau <tchau2@outlook.com>
2017-11-29 12:47:48 -0500

    Initial files and code for html, css, and js

---
+++ assets/js/javascript.js
@@ -0,0 +1,15 @@
+  $(".placeholder").on("click", function() {
+  	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+			url += '?' + $.param({
+  	'api-key': "8db7e5f2f14e40afb811ff718ef5bd56"
+		});
+		$.ajax({
+  		url: url,
+  		method: 'GET',
+  	}).done(function(result) {
+  		console.log(result);
+		}).fail(function(err) {
+  		throw err;
+		});
+  });
+  

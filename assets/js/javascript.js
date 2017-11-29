74e022da497e10f86b1b8d46b4a65200b6422f5f
kelichau <tchau2@outlook.com>
2017-11-29 13:15:11 -0500

    Updated javascript

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,15 +1,26 @@
   $(".placeholder").on("click", function() {
-  	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-			url += '?' + $.param({
-  	'api-key': "8db7e5f2f14e40afb811ff718ef5bd56"
-		});
-		$.ajax({
-  		url: url,
-  		method: 'GET',
-  	}).done(function(result) {
-  		console.log(result);
-		}).fail(function(err) {
-  		throw err;
-		});
+
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"; url += '?' + $.param({ 'api-key': "8db7e5f2f14e40afb811ff718ef5bd56"
   });
   
+  console.log(url);
+
+  $.ajax({
+    url: url,
+    method: 'GET',
+  }).done(function(result) {
+  
+  console.log(result);
+  
+  }).fail(function(err) {
+    throw err;
+    });
+  });
+
+  $(document).ready(function(){
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+      url += '?' + $.param({
+      'api-key': "8db7e5f2f14e40afb811ff718ef5bd56"
+    });
+    console.log(url);
+  })
